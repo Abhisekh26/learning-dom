c2b6031324b50e8baa7fd9c75dc89139049c2cc7
Abhisekh26 <abhisekhjimmy@gmail.com>
2023-09-15 11:07:24 +0530

    selecting dom

---
+++ random.js
@@ -0,0 +1,7 @@
+var x=document.getElementById("header-title")
+var z=document.getElementById("main-header")
+x.textContent="hellog"
+z.style.borderBottom= 'solid 3px #000'
+  var y=document.querySelector("h2")
+ y.style.fontStyle="italic"
+ y.style.color= "#00FF00"
